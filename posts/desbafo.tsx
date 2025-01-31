da481356ae78bb6dd0de871c6cac12e310a9d59a
André Lucas <2007andredoe@gmail.com>
2025-01-31 15:15:17 -0300

    Create desbafo.tsx

---
+++ posts/desbafo.tsx
@@ -0,0 +1,9 @@
+---
+title: WIP Desabafo sincero
+date: 2025/1/31
+description: ADG falando absolutamente qualquer coisa
+tag: desabafo
+author: ADG
+fixed: true
+---
+Sendo sincero, tem um tempinho que eu ando querendo escrever isso, mas eu acho... Triste a situação que 
